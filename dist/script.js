6ddda5acbf67609d81735fc949d3343988b013cf
hamiecod <hargunbeersingh@gmail.com>
2021-06-22 17:33:17 +0530

    :sparkles: Added game over screen and retry button

--- dist/script.js
+++ dist/script.js
@@ -1,217 +1,157 @@
-// variables
-
-// game components
-var dino = document.querySelector(".game-dino");
-var obstacle = document.querySelector(".game-obstacle");
-var gameOver = document.querySelector(".game-over");
-var scoreContainer = document.querySelector(".game-score__container");
-
-// audio
-var gameAudio = new Audio("../assets/music.mp3");
-var gameOverAudio = new Audio("../assets/death.mp3");
-
-// splash screen
-var playButton = document.querySelector(".splash-play__button");
-var splash = document.querySelector(".splash");
-
-// game values
-var score = 0;
-// score of the player
-var cross = true;
-// whether the obstacle has crossed the dragor
-// whether the dragon has jumped over the obstacle
-  // if cross is false it means that the obstacle is under the dino or just passing the dino
-  // if cross is true it means that the obstacle is not very near the dino and is not passing the dino and is somewhere else on the playground
-
-// game component position values
-dinoX = parseInt(
-  window.getComputedStyle(dino, null)
-  .getPropertyValue("left")
-);
-// X axis value of dino
-
-dinoY = parseInt(
-  window.getComputedStyle(dino, null)
-  .getPropertyValue("top")
-);
-// Y axis value of dino
-
-obstacleX = parseInt(
-  window.getComputedStyle(obstacle, null).getPropertyValue("left")
-);
-// X axis value of obstacle
-
-obstacleY = parseInt(
-  window.getComputedStyle(obstacle, null).getPropertyValue("top")
-);
-// Y axis value of obstacle
-
-distanceX = Math.abs(dinoX - obstacleX);
-// X axis distance between the dino and the obstacle
-
-distanceY = Math.abs(dinoY - obstacleY);
-// Y axis distance between the dino and the obstacle
-
-animationDuration = parseFloat(
-  window.getComputedStyle(obstacle, null).getPropertyValue("animation-duration")
-);
-// the duration of the obstacle animation AKA the obstacle speed
-// default value = 3s
-
+score = 0;
+cross = true;
+var playButton = document.querySelector('.splash-play__button');
 var newDuration;
-// the new animation duration of the obstacle AKA the new obstacle speed
-
 // variables declared without let or var keyword become global and can be accessed anywhere
 
-// splash screen
-playButton.addEventListener("click", hideSplash);
-// the hideSplash function is started on the play button click
+gameOver = document.querySelector(".game-over");
+gameOver.classList.add("hidden");
+gameOver.classList.remove("visible");
 
-splash.classList.add("visible");
-// makes the splash screen visible
-obstacle.classList.add("hidden");
-// hides the obstacle so that it is not visible behind the splash screen
-
-// function to hide the splash screen
-function hideSplash() {
-  splash.classList.remove("visible");
-  // removes the visible class
-  splash.classList.add("hidden");
-  // hides the splash screen
-  setTimeout(() => {
-    startGame();
-  }, 1500);
-  // starts the game in 1500ms, so that the player can set himself to the environment
-}
-
-
-// starts the main game
 function startGame() {
-  setTimeout(() => {
-    gameAudio.play();
-  }, 300);
-  // audio is turned on, 300ms after the play button click
-  // audio is played after a delay so that the user can see the environment and then get into the play mode
+  score=0;
+  updateScore(score);
 
+  dino = document.querySelector(".game-dino");
+  dino.style.left= 3.5 + "vw";
+  dino.style.transform = "scaleX(1)";
+
+  gameAudio = new Audio('../assets/music.mp3');
+  gameOverAudio = new Audio('../assets/death.mp3');
   obstacle.classList.remove("hidden");
   obstacle.classList.add("visible");
-  // makes the obstacle visible
-  // the obstacle is hidden due to splash screen
-
-  obstacle.style.right = -3 + "vw";
-  // takes the obstacle out of the screen to prevent random spawning of the obstacle
+  obstacle.classList.add("obstacleAnimation");
+  obstacle.style.right = -10 + "vw";
+  gameOver = document.querySelector(".game-over");
   gameOver.classList.add("hidden");
-  // hides the game over message(default)
+  setTimeout(() => {
+    gameAudio.play();
+  }, 1200);
 
-
-  // PLAYER CONTROLS 
   // onkeydown is triggered when a key is pressed
   // e.keycode will give the key code of the key pressed
   document.onkeydown = function (e) {
+    dino = document.querySelector(".game-dino");
+    // console.log(e.keyCode);
     if (e.keyCode == 87 || e.keyCode == 38 || e.keyCode == 32) {
       // JUMP
       // 87 is w
       // 38 is up arrow key
       // 32 is space bar
       dino.classList.add("animateDino");
-      // animates the dino(player) to jump when specific key is pressed, see _game.scss
       setTimeout(() => {
         dino.classList.remove("animateDino");
-        // stops the dino animation after 800 ms, thus stopping the dino jump
-      }, 800);
-    } 
-    else if (e.keyCode == 68 || e.keyCode == 39) {
+      }, 1100);
+    } else if (e.keyCode == 68 || e.keyCode == 39) {
       // RIGHT
       // 68 is d
       // 39 is right arrow key
+      dinoX = parseInt(
+        window.getComputedStyle(dino, null).getPropertyValue("left")
+      );
       dino.style.transform = "scaleX(1)";
-      // makes the dino look towards the right
-      dino.style.left = dinoX + 8.75 + "vw";
-      // makes the dino walk towards the right
-    } 
-    else if (e.keyCode == 65 || e.keyCode == 37) {
+      dino.style.left = dinoX + 112 + "px";
+    } else if (e.keyCode == 65 || e.keyCode == 37) {
       // LEFT
       // 65 is a
       // 37 is left arrow key
+      dinoX = parseInt(
+        window.getComputedStyle(dino, null).getPropertyValue("left")
+      );
       dino.style.transform = "scaleX(-1)";
-      // makes the dino look towards right
-      dino.style.left = dinoX - 8.75 + "vw";
-      // makes the dino move towards left
+      dino.style.left = dinoX - 112 + "px";
     }
   };
 
   setInterval(() => {
-    // every 100ms check the following:
-    if (distanceX < 120 && distanceY < 30) {
-      // if the distance between the dino and obstacle on the X axis is less than 120px
-      // if the distance between the dino and obstacle on the Y axis is less than 30px
-      // it just acts like `is touching` function
-      // if the distance between the dino and obstacle on X axis is less than 120px, they both are touching each other
-      // if the distance between the dino and obstacle on X axis is less than 120px as well as the distance between the both on the Y axis is less than 30px, they are touching each other
+    dino = document.querySelector(".game-dino");
+    gameOver = document.querySelector(".game-over");
+    obstacle = document.querySelector(".game-obstacle");
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue("left"));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue("top"));
+
+    ox = parseInt(
+      window.getComputedStyle(obstacle, null).getPropertyValue("left")
+    );
+    oy = parseInt(
+      window.getComputedStyle(obstacle, null).getPropertyValue("top")
+    );
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+
+    if (offsetX < 120 && offsetY < 30) {
       obstacle.classList.remove("obstacleAnimation");
-      // stops the obstacle movement AKA the obstacle animation
       gameOver.classList.remove("hidden");
       gameOver.classList.add("visible");
-      // makes the Game Over visible
 
       gameAudio.pause();
-      // pauses the game audio
-
       // death audio
       gameOverAudio.play();
-      // plays the death audio
-
       setTimeout(() => {
         gameOverAudio.pause();
-      }, 1500);
-      // stops the death audio after 1.5s
-
-    } 
-    else if (distanceX > 180 && distanceX < 250 && cross) {
-      // if the distance between the dino and obstacle on the X axis is in between 180px and 250px
-      // if cross is true
+      }, 1500);  
+      
+      // game over score
+      scoreContainer = document.querySelector('.game-score__container');
+      gameOverScore = document.querySelector('.game-over__score');
+      gameOverScore.innerHTML = "Your Score: "+ score;
+    } else if (offsetX > 180 && offsetX < 250 && cross) {
       score += 1;
-      // increment the score by 1
       updateScore(score);
-      // calls the updateScore function with the score variable as the parameter
-
-      // cross varaible defines whether the variable is passing the dino or not
-      // if cross is false it means that the obstacle is under the dino or just passing the dino
-      // if cross is true it means that the obstacle is not very near the dino and is not passing the dino and is somewhere else on the playground
       cross = false;
-      // sets cross to false
-
       setTimeout(() => {
         cross = true;
       }, 1000);
-      // sets cross to true after 1s, when the obstacle has passed the dino
-
       setTimeout(() => {
-        if (newDuration >= 1.2) {
-          // if the value of aniation Duration is greater than 1.2s
-          // default value of animationDuration = 3s;
+        animationDuration = parseFloat(
+          window
+            .getComputedStyle(obstacle, null)
+            .getPropertyValue("animation-duration")
+        );
+        console.log(animationDuration);
+        if (animationDuration >= 1.2) {
           newDuration = animationDuration - 0.05;
-          // everytime the dino passes the obstacle the speed of the obstacle increases
-          // the animation duration of the obstacle is decreased by 0.05s so it appears to move faster everytime the dino crosses it
+          console.log(newDuration);
           obstacle.style.animationDuration = newDuration + "s";
-          // sets the animation duration to the calculated new duration of the animation and adds a 's' at the end of the value, depicting seconds
-        } 
-        else if (newDuration <= 1.2) {
-          // if the animation duration is less than 1.2s
-          // the animation duration doesnt increase if the animation duration is lesser or equal to 1.2
-          // the speed of the obstacle doesnt increase after it has reached 1.2s because it would be extremely difficult for the player to handle such high speed
-          // console.log("top speed reached");
+        } else if (newDuration <= 1.2) {
+          console.log("top speed reached");
         }
-      }, 340);
-      // this is done 340ms after the obstacle has passed the dino because if we increase the speed while the obstacle is passing the dino, it would cause a jerk, distracting the player at the crucial moment
+      }, 345);
     }
-  }, 100)
-  // this all is checked every 100ms
-  // every 100ms it is checked whether the obstacle is touching the dino
+  }, 100);
+
+
 }
 
 function updateScore(score) {
-  // this function updates the score in the DOM
+  scoreContainer = document.querySelector(".game-score__container");
   scoreContainer.innerHTML = "Your Score: " + score;
-  // the parameter supplied is set as the score
+}
+
+function hideSplash(){
+  let splash = document.querySelector('.splash');
+  splash.classList.remove("visible");
+  splash.classList.add("hidden");
+  setTimeout(() => {
+    startGame();
+  }, 1500);
+}
+
+playButton.addEventListener('click', hideSplash);
+let obstacle = document.querySelector('.game-obstacle');
+let splash = document.querySelector('.splash');
+splash.classList.add("visible");
+obstacle.classList.add("hidden");
+
+// retry
+let retry = document.querySelector('.game-over__retry-button');
+retry.addEventListener('click', startGame);
+retry.addEventListener('click', hideGameOver);
+
+function hideGameOver(){
+  gameover = document.querySelector('.game-over');
+  gameover.classList.add('hidden');
+  gameover.classList.remove('visible');
 }
